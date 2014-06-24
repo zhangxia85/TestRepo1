f6fbb1a4f93df15d0b102e51b24c2be8f2833d4b
zhangxia85 <zhangxia.finance@live.cn>
2014-06-24 17:32:22 +0800

    initial submit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -0,0 +1,9 @@
+﻿// 在 http://fsharp.net 上了解有关 F# 的更多信息
+// 请参阅“F# 教程”项目以获取更多帮助。
+open System;
+[<EntryPoint>]
+let main argv = 
+    Console.WriteLine "Hello F#!"
+    Console.WriteLine "Press ENTER to exit."
+    Console.ReadLine() |> ignore
+    0 // 返回整数退出代码
