f50f9ef70f061a4467e35bdcdd1172f9058b7f20
zhangxia85 <zhangxia.finance@live.cn>
2014-06-24 17:49:51 +0800

    alias Console.WriteLine

--- ConsoleApplication1/ConsoleApplication1/Program.fs
+++ ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -1,9 +1,10 @@
 ﻿// 在 http://fsharp.net 上了解有关 F# 的更多信息
 // 请参阅“F# 教程”项目以获取更多帮助。
 open System;
+let wl = Console.WriteLine :string->unit
 [<EntryPoint>]
 let main argv = 
-    Console.WriteLine "Hello F#!"
-    Console.WriteLine "Press ENTER to exit."
+    wl "Hello F#!"
+    wl "Press ENTER to exit."
     Console.ReadLine() |> ignore
     0 // 返回整数退出代码
