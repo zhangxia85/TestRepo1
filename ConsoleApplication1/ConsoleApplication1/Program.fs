7e44ee0743c850f70e59b048f1435cef489f1c92
zhangxia85 <zhangxia.finance@live.cn>
2014-06-26 14:26:56 +0800

    alias ReadLine

--- ConsoleApplication1/ConsoleApplication1/Program.fs
+++ ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -1,10 +1,11 @@
-﻿// 在 http://fsharp.net 上了解有关 F# 的更多信息
-// 请参阅“F# 教程”项目以获取更多帮助。
-open System;
-let wl = Console.WriteLine :string->unit
+﻿open System
+
+let wl : string -> unit = Console.WriteLine
+let rl = Console.ReadLine
+
 [<EntryPoint>]
 let main argv = 
     wl "Hello F#!"
     wl "Press ENTER to exit."
-    Console.ReadLine() |> ignore
-    0 // 返回整数退出代码
+    rl() |> ignore
+    0
