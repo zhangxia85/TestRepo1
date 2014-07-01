a458669c52f04ab6fcba944c30753b537d9bfdc7
zhangxia85 <zhangxia.finance@live.cn>
2014-07-01 16:30:59 +0800

    alias Console etc

--- ConsoleApplication1/ConsoleApplication1/Program.fs
+++ ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -1,11 +1,17 @@
-﻿open System
+﻿type c = System.Console
 
-let wl : string -> unit = Console.WriteLine
-let rl = Console.ReadLine
+type cc = System.ConsoleColor
+
+let rc = c.ResetColor
+let wl : string -> unit = c.WriteLine
+let rl = c.ReadLine
 
 [<EntryPoint>]
 let main argv = 
+    c.BackgroundColor <- cc.Cyan
+    c.ForegroundColor <- cc.DarkRed
     wl "Hello F#!"
+    rc()
     wl "Press ENTER to exit."
     rl() |> ignore
     0
